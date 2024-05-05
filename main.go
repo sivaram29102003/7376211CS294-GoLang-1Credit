cad38ed9a9af8faeca81747cdc2fa2f69d361be1
sivaram M R <sivaramramakrishnan29@gmail.com>
2024-05-05 16:10:44 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+	http.HandleFunc("/name", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Sivaram M R")
+		
+	})
+	http.HandleFunc("/rollno", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "7376211cs294")
+		
+	})
+
+	fmt.Printf("Server running (port=8080), route: http://localhost:8080/helloworld\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
